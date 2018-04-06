715feb6f055f7f03d9feac4886998b354a7ccd69
Jay <jlincoln88@gmail.com>
2018-04-05 22:13:18 -0400

    leave user class in app namespace

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-// TODO change namespace of user model
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,5 +1,5 @@
 <?php
-// TODO handle this file
+
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
