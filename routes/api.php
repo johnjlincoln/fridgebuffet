51acf619b46915b116993448e512c80ee2183161
Jay <jlincoln88@gmail.com>
2018-04-08 00:58:56 -0400

    comment on api routes

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// TODO: implement auth
+// Route::middleware('auth:api')->get('/get/newRecipes', 'API\apiController@getNewRecipes');
 Route::get('/get/newRecipes', 'API\apiController@getNewRecipes');
 Route::get('/get/recipeData', 'API\apiController@getRecipeData');
