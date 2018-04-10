9d12b1d95a7645f0e171136c7cc487e6230a8584
Jay <jlincoln88@gmail.com>
2018-04-10 02:25:43 -0400

    comments

--- app/Jobs/apiGetNewRecipeData.php
+++ app/Jobs/apiGetNewRecipeData.php
@@ -1,19 +1,27 @@
 <?php
 /**
-* TODO: doc comment
-* TODO: Comment Inline
+* PHP Script to handle getting data for recipes from the Food2Fork API and sending it to fridgebuffet.
 *
 * @author John J Lincoln <jlincoln88@gmail.com>
 * @copyright 2018 Arctic Pangolin
 */
 
-//have to load env somehow
+/**
+ * Script Arguments
+ * These MUST be passed in when executing this script to ensure functionality
+ *
+ * Argument 1 - Base URL of the application
+ * Argument 2 - F2F API Key
+ *
+ * @example php /path/to/apiGetNewRecipeData.php http://www.fridgebuffet.com 1337s3Kr3tAp1k3y1337
+ * @var array
+ */
 $script_arguments = [
     'base_url'    => !empty($argv[1]) ? $argv[1] : 'localhost',
     'f2f_api_key' => !empty($argv[2]) ? $argv[2] : '',
 ];
 
-// Configure cURL for an internal API call
+// Configure cURL to request an unpulled apiRecie from the fridgebuffet apiRecipes
 $defaults = [
     CURLOPT_URL            => $script_arguments['base_url'] . '/api/get/unpulledApiRecipe',
     CURLOPT_RETURNTRANSFER => true
@@ -25,20 +33,21 @@ curl_setopt_array($ch, $defaults);
 $response = curl_exec($ch);
 $response = json_decode($response);
 
-// Check for errors and display the error message
+// Check for errors and display the error message - die if error
 if ($errno = curl_errno($ch)) {
     $error_message = curl_strerror($errno);
     echo "cURL error ({$errno}):\n {$error_message}";
+    die();
 }
 
 // Close the handle
 curl_close($ch);
 
-// lod if succes
+// Save the apiRecipe ID and corresponding Food2Fork ID
 $unpulled_api_f2f_id = $response->api_f2f_id;
 $unpulled_api_id = $response->api_id;
 
-// Configure cURL for API call to Food2Fork.
+// Configure cURL for API call to Food2Fork
 $params = [
     'key' => $script_arguments['f2f_api_key'],
     'rId' => $unpulled_api_f2f_id
@@ -50,17 +59,19 @@ $defaults = [
     CURLOPT_POSTFIELDS     => $params,
     CURLOPT_RETURNTRANSFER => true
 ];
+
 $ch = curl_init();
 curl_setopt_array($ch, $defaults);
 
-// Send request
+// Send request and load response
 $response = curl_exec($ch);
 $response = json_decode($response);
 
-// Check for errors and display the error message
+// Check for errors and display the error message - die if error
 if ($errno = curl_errno($ch)) {
     $error_message = curl_strerror($errno);
     echo "cURL error ({$errno}):\n {$error_message}";
+    die();
 }
 
 if (!isset($response)) {
@@ -71,10 +82,10 @@ if (!isset($response)) {
 // Close the handle
 curl_close($ch);
 
-//lod if succ
+// Save the new recipe data
 $ingredients = $response->recipe->ingredients;
 
-// Configure cURL
+// Configure cURL to send a payload of recipe data to the fridgebuffet API
 $new_api_recipe_data = [
     'api_f2f_id'  => $unpulled_api_f2f_id,
     'api_id'      => $unpulled_api_id,
@@ -86,7 +97,7 @@ $http_header = [
     'Content-Type: application/json',
     'Content-Length: ' . strlen($payload)
 ];
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
 $defaults = [
     CURLOPT_URL            => $script_arguments['base_url'] . '/api/post/apiRecipeData',
     CURLOPT_POST           => true,
@@ -94,22 +105,25 @@ $defaults = [
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER     => $http_header
 ];
+
 $ch = curl_init();
 curl_setopt_array($ch, $defaults);
 
-// Send request
+// Send request and load response
 $response = curl_exec($ch);
-$response = json_decode($response);
 
-// Check for errors and display the error message
+// Check for errors and display the error message - die if error
 if ($errno = curl_errno($ch)) {
     $error_message = curl_strerror($errno);
     return response()->json([
         'error' => "cURL error ({$errno}):\n {$error_message}"
         ]);
+    die();
 }
 
-echo isset($response) ? var_dump($response) : 'Error - Check data';
+// Echo the json response of the fridgebuffet endpoint
+echo $response;
 
-// Close the handle and load the response
+// Close the handle and die
 curl_close($ch);
+die();
