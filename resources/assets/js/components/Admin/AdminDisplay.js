e7f42d00ba8eebc144853ea0475f807b279fc124
Jay <jlincoln88@gmail.com>
2018-04-14 18:43:32 -0400

    thing

--- resources/assets/js/components/Admin/AdminDisplay.js
+++ resources/assets/js/components/Admin/AdminDisplay.js
@@ -2,27 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 const test = () => {
-    console.log('flerp');
+    console.log('pew pew');
 };
 
-const AdminDisplay = props => (
-    <div className="container">
-        <div className="row justify-content-center">
-            <div className="col-md-8">
-                <div className="card">
-                    <div className="card-header">Admin Dashboard</div>
-                    <div className="card-body">
-                        <button onClick={test}>Activate Lasers</button>
+const AdminDisplay = props => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Admin Dashboard</div>
+                        <div className="card-body">
+                            <button onClick={test}>Activate Lasers</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 AdminDisplay.propTypes = {
     handleTest: PropTypes.func
-}
+};
 
 AdminDisplay.defaultProps = {
     //
