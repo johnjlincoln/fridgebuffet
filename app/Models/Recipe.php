02f4ffbb9b08d335dc7f279ef6663ec12a90362c
Jay <jlincoln88@gmail.com>
2018-04-26 00:33:15 -0400

    block comments on models

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Main Recipe Model
+ *
+ * @author John J Lincoln <jlincoln88@gmail.com>
+ * @copyright 2018 Arctic Pangolin
+ */
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
